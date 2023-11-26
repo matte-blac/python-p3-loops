330ccf2645af8695a887440bc52e613391141f0e
Matthew Bomett <bomettmat2@gmail.com>
2023-11-26 22:16:59 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print (i)
+        print ('Happy New Year!')
+        i -= 1
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_ints = [square ** 2 for square in int_list]
+    print (squared_ints)
+    squared_ints == None
+    return squared_ints
+pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0:
+            print('Fizz')    
+        else:
+            print(i)  
     pass
